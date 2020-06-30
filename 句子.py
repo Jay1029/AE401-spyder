fcaa0030541f052c55be2d534d98112b3a890f82
Jay1029 <65231411+Jay1029@users.noreply.github.com>
2020-06-30 18:04:08 +0800

    句子

---
+++ 句子.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 17:30:27 2020
+
+@author: User
+"""
+import random
+name = []
+verb = []
+noun = []
+name=('Jay','mark','kyle')
+verb=('run','play','sleep')
+noun=('Taipei','New Taipei','Hsinchu')
+a = name
+a = random.randint('Jay','kyle')
+b = verb
+b = random.randint('run','sleep')
+c = noun
+c = random.randint('Taipei','Hsinchu')
+print(a,b,c)

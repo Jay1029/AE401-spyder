bb4a0847f1f3bf7a7d945ecab11d9675990d1694
Jay1029 <65231411+Jay1029@users.noreply.github.com>
2020-07-14 16:01:42 +0800

    未完成字典

--- wwww.py
+++ wwww.py
@@ -4,15 +4,15 @@ Created on Tue Jun 30 19:13:52 2020
 
 @author: User
 """
-w=[]
 ch=[]
 eng=[]
-words=[]
+words={}
 answer=[]
 
 print('########################')
 print('#Python字典#')
 print('########################')
+
 
 while True:
     
@@ -25,19 +25,46 @@ while True:
     print('6. 離開系統')
     
     sel = input('請輸入欲執行選項: ')
-
     if sel=='1': 
-        w = input('輸入單字:')
+        while True:
+            w = input("輸入新單字(按0退出)")
+            if w =='0':
+                break
+            if w not in words:
+                m = input('中文解釋:') 
+                words[w] = m
+        else:    
+            break
     elif sel=='2': 
-        print(w)
+        us = sorted(words)
+        for i in us:
+            print(i,"是",words[i])
     elif sel=='3': 
-        ch = input('填入中文')
-    elif sel=='4':
         eng = input('填入英文')
-    elif sel=='5': 
-        answer=('w','填入中文'
-              'ch,填入英文')
-        if answer =('ch''eng')
+        if eng in words:
+            print('中文:',words[eng])
+    elif sel=='4':
+        got=False
+        ch = input('輸入要查詢的中文(按0跳出): ')
+        if ch == '0':
+            break
+        for k,v in d.items():
+            if ch==v :
+                print(ch,'的英文是',k)
+                got=True
+        if not got:
+            print('抱歉，查不到!')    
+
+    elif sel=='5':
+        score=0
+        print(v,':')
+        ans = input()
+        if ans == k:
+            score +=1
+            print('正確')  
+        else:
+            print('錯誤')
+    elif sel=='6':
         break
     else:
         print('wrong input')
